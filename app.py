7963c2a197e9a75c778add7913433797f5e2b89c
Luis Torres Cereño <torrescereno@gmail.com>
2023-11-24 01:18:24 -0300

    Módulo de chat constitucional

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 import os
 
+import pinecone  # noqa
 import streamlit as st
 
 from dotenv import load_dotenv
 
 # from langchain.agents import AgentType, create_pandas_dataframe_agent
-from langchain.agents import AgentType
-from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain.callbacks import StreamlitCallbackHandler
+# from langchain.agents import AgentType
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.pinecone import Pinecone
+
+# from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+# from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import (
     ConversationBufferMemory,
     StreamlitChatMessageHistory,
 )
-
 from callback import PrintRetrievalHandler, StreamHandler
-from config import configure_retriever, load_data
-from utils import file_formats_dataframe, files_formats_documents, clear_submit
+# from config import configure_retriever, load_data
+# from utils import file_formats_dataframe, files_formats_documents, clear_submit
 
 load_dotenv()
 
@@ -73,43 +76,16 @@ if type_process == "Constitución":
 
     title.title("Chat constitucional")
 
-    # -----------------------------
-    # Obtener los vectores de pinecone
-
-    # -----------------------------
-
-    msgs = StreamlitChatMessageHistory()
-
-    if len(msgs.messages) == 0 or st.sidebar.button("Limpiar historial"):
-        msgs.clear()
-        msgs.add_ai_message("En qué puedo ayudarte?")
-
-    for msg in msgs.messages:
-        st.chat_message(avatars[msg.type]).write(msg.content)
-
-    if user_query := st.chat_input(placeholder="Escribe tu pregunta aquí"):
-        st.chat_message("user").write(user_query)
-
-        with st.chat_message("assistant"):
-            retrieval_handler = PrintRetrievalHandler(st.container())
-            stream_handler = StreamHandler(st.empty())
-            # response = qa_chain.run(
-            #     user_query, callbacks=[retrieval_handler, stream_handler]
-            # )
-
-
-if type_process == "Documentos":
-    uploaded_files = st.sidebar.file_uploader(
-        label="Sube tus archivos", type=files_formats_documents, accept_multiple_files=True
+    pinecone.init(
+        api_key=os.getenv("PINECONE_API_KEY"),
+        environment=os.getenv("PINECONE_ENV"),
     )
 
-    if not uploaded_files:
-        st.info(
-            "Por favor sube un documento para continuar. Al cargar los archivos iniciará el proceso"
-        )
-        st.stop()
+    index_name = "constitution-idx"
 
-    retriever = configure_retriever(files=uploaded_files, api_key=openai_api_key)
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
     msgs = StreamlitChatMessageHistory()
 
@@ -124,14 +100,16 @@ if type_process == "Documentos":
         streaming=True,
     )
 
-    # por aca pasamos el template
-    qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        memory=memory,
+        chain_type="stuff",
+    )
 
     if len(msgs.messages) == 0 or st.sidebar.button("Limpiar historial"):
         msgs.clear()
         msgs.add_ai_message("En qué puedo ayudarte?")
-
-    avatars = {"human": "user", "ai": "assistant"}
 
     for msg in msgs.messages:
         st.chat_message(avatars[msg.type]).write(msg.content)
@@ -144,51 +122,97 @@ if type_process == "Documentos":
             stream_handler = StreamHandler(st.empty())
             response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
 
-if type_process == "Dataframe":
-    df = None
-
-    uploaded_file = st.sidebar.file_uploader(
-        "Sube tus archivos",
-        type=list(file_formats_dataframe.keys()),
-        on_change=clear_submit,
-    )
-
-    if not uploaded_file:
-        st.info(
-            "Por favor sube un documento para continuar. Al cargar los archivos iniciará el proceso"
-        )
-        st.stop()
-
-    if uploaded_file:
-        df = load_data(uploaded_file)
-
-    if "messages" not in st.session_state or st.sidebar.button("Limpiar historial"):
-        st.session_state["messages"] = [
-            {"role": "assistant", "content": "En qué puedo ayudarte?"}
-        ]
-
-    for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
-
-    if prompt := st.chat_input(placeholder="Que quieres saber de los datos?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.chat_message("user").write(prompt)
-
-        llm = ChatOpenAI(
-            temperature=0,
-            model="gpt-3.5-turbo-0613",
-            openai_api_key=openai_api_key,
-            streaming=True,
-        )
-
-        pandas_df_agent = create_pandas_dataframe_agent(
-            llm=llm,
-            df=df,
-            agent_type=AgentType.OPENAI_FUNCTIONS,
-        )
-
-        with st.chat_message("assistant"):
-            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-            response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
-            st.session_state.messages.append({"role": "assistant", "content": response})
-            st.write(response)
+# if type_process == "Documentos":
+#     uploaded_files = st.sidebar.file_uploader(
+#         label="Sube tus archivos", type=files_formats_documents, accept_multiple_files=True
+#     )
+#
+#     if not uploaded_files:
+#         st.info(
+#             "Por favor sube un documento para continuar. Al cargar los archivos iniciará el proceso"
+#         )
+#         st.stop()
+#
+#     retriever = configure_retriever(files=uploaded_files, api_key=openai_api_key)
+#
+#     msgs = StreamlitChatMessageHistory()
+#
+#     memory = ConversationBufferMemory(
+#         memory_key="chat_history", chat_memory=msgs, return_messages=True
+#     )
+#
+#     llm = ChatOpenAI(
+#         model_name=selected_model,
+#         openai_api_key=openai_api_key,
+#         temperature=temperature,
+#         streaming=True,
+#     )
+#
+#     # por aca pasamos el template
+#     qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
+#
+#     if len(msgs.messages) == 0 or st.sidebar.button("Limpiar historial"):
+#         msgs.clear()
+#         msgs.add_ai_message("En qué puedo ayudarte?")
+#
+#     avatars = {"human": "user", "ai": "assistant"}
+#
+#     for msg in msgs.messages:
+#         st.chat_message(avatars[msg.type]).write(msg.content)
+#
+#     if user_query := st.chat_input(placeholder="Escribe tu pregunta aquí"):
+#         st.chat_message("user").write(user_query)
+#
+#         with st.chat_message("assistant"):
+#             retrieval_handler = PrintRetrievalHandler(st.container())
+#             stream_handler = StreamHandler(st.empty())
+#             response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+#
+# if type_process == "Dataframe":
+#     df = None
+#
+#     uploaded_file = st.sidebar.file_uploader(
+#         "Sube tus archivos",
+#         type=list(file_formats_dataframe.keys()),
+#         on_change=clear_submit,
+#     )
+#
+#     if not uploaded_file:
+#         st.info(
+#             "Por favor sube un documento para continuar. Al cargar los archivos iniciará el proceso"
+#         )
+#         st.stop()
+#
+#     if uploaded_file:
+#         df = load_data(uploaded_file)
+#
+#     if "messages" not in st.session_state or st.sidebar.button("Limpiar historial"):
+#         st.session_state["messages"] = [
+#             {"role": "assistant", "content": "En qué puedo ayudarte?"}
+#         ]
+#
+#     for msg in st.session_state.messages:
+#         st.chat_message(msg["role"]).write(msg["content"])
+#
+#     if prompt := st.chat_input(placeholder="Que quieres saber de los datos?"):
+#         st.session_state.messages.append({"role": "user", "content": prompt})
+#         st.chat_message("user").write(prompt)
+#
+#         llm = ChatOpenAI(
+#             temperature=0,
+#             model="gpt-3.5-turbo-0613",
+#             openai_api_key=openai_api_key,
+#             streaming=True,
+#         )
+#
+#         pandas_df_agent = create_pandas_dataframe_agent(
+#             llm=llm,
+#             df=df,
+#             agent_type=AgentType.OPENAI_FUNCTIONS,
+#         )
+#
+#         with st.chat_message("assistant"):
+#             st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+#             response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
+#             st.session_state.messages.append({"role": "assistant", "content": response})
+#             st.write(response)
